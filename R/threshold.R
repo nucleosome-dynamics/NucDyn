efd250fc5480caf02d9833c2b77551c9ba0daf52
rilla <r.illa.pujagut@gmail.com>
2016-07-07 15:48:57 +0200

    zeroes bug fixed

--- R/threshold.R
+++ R/threshold.R
@@ -1,4 +1,4 @@
-.dfToLine <- function(df, max.vals, chr=NULL, expand=30) {
+.dfToLine <- function(df, max.val, expand=30) {
     cWrap <- function (vals, max.val, expand=0) {
         start <- vals$start - expand
         start[start < 1] <- 1
@@ -17,31 +17,67 @@
                    max.val, out=out)$out
         return(cOut)
     }
-
-    if (is.null(chr)) {
-        chr <- df[1, "chr"]
-    }
-    max.val <- max.vals[[chr]]
-
     nonzero <- df[df$nreads != 0, ]
     xs <- cWrap(nonzero, max.val, expand)
-    filterFFT(xs,
-              useOptim=TRUE,
-              pcKeepComp=0.005)
+    res <- filterFFT(xs,
+                     useOptim=TRUE,
+                     pcKeepComp=0.005)
+    res[res < 1] <- 1
+    res
 }
+
+#.dfToLine <- function(df, max.vals, chr=NULL, expand=30) {
+#    cWrap <- function (vals, max.val, expand=0) {
+#        start <- vals$start - expand
+#        start[start < 1] <- 1
+#        start <- as.integer(start)
+#        end <- vals$end + expand
+#        end[end > max.val] <- max.val
+#        end <- as.integer(end)
+#        xs <- as.integer(vals$nreads)
+#        n <- as.integer(length(xs))
+#        out <- as.numeric(rep(0, max.val))
+#        cOut <- .C("cov_ranges",
+#                   xs, start, end, n,
+#                   max.val, out=out)$out
+#        return(cOut)
+#    }
+#    if (is.null(chr)) {
+#        chr <- df[1, "chr"]
+#    }
+#    max.val <- max.vals[[chr]]
+#    nonzero <- df[df$nreads != 0, ]
+#    xs <- cWrap(nonzero, max.val, expand)
+#    filterFFT(xs,
+#              useOptim=TRUE,
+#              pcKeepComp=0.005)
+#}
 
 .averageThreshs <- function (...)
     .nmapply(function (...) .nmapply(.vectorMean, ...), ...)
 
-.processHs <- function (hs, maxs, mc.cores=1)
-    .xdlply_rep(hs,
-                "chr",
-                function (x)
-                    .dlplyf(x,
-                            .typeSplitter,
-                            .dfToLine,
-                            maxs),
-                mc.cores=mc.cores)
+.processHs <- function (hs, maxs, mc.cores=1) {
+    chrs <- unique(hs$chr)
+    res <- .xlapply(chrs,
+                    function (chr)
+                        .dlplyf(hs[hs$chr == chr, ],
+                                .typeSplitter,
+                                .dfToLine,
+                                maxs[[chr]]),
+                    mc.cores=mc.cores)
+    names(res) <- chrs
+    res
+}
+
+#.processHs <- function (hs, maxs, mc.cores=1)
+#    .xdlply_rep(hs,
+#                "chr",
+#                function (x)
+#                    .dlplyf(x,
+#                            .typeSplitter,
+#                            .dfToLine,
+#                            maxs),
+#                mc.cores=mc.cores)
 
 variableThreshFromHss <- function (..., mc.cores=1)
 {
@@ -114,7 +150,7 @@ setMethod(
         applyThreshVect <- function (hs, t) {
             diff <- max(hs$coord) - length(t)
             if (diff > 0) {
-                t <- c(t, rep(0, diff))
+                t <- c(t, rep(1, diff))
             }
             hs[hs$nreads >= (t[hs$coord] * scale), ]
         }
