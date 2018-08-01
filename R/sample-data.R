93884c95406c3e02cc2d075a4ff795759fe5ab68
Diana Buitrago <dianacamila@gmail.com>
2018-08-01 11:25:58 +0200

    Modifications for Bioconductor checks (Diego)

---
+++ R/sample-data.R
@@ -0,0 +1,12 @@
+#' Sample chromosome II coverage data
+#'
+#' Data from a MNaseSeq experiment in yeast in the following mitosis phases:
+#' G2 and M
+#'
+#' @docType data
+#'
+#' @usage data(sample_chrII)
+#'
+#' @format An object of class list
+#'
+"sample_chrII" 
